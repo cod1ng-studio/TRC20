84e699971146a100581e6d27d69294aea35c2808
tolya-yanot <tolyayanot@gmail.com>
2020-03-20 14:45:36 +0300

    delete sh files

--- test_dex.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-~/liteclient-build/crypto/func -P -o token.fif /Users/tolyayanot/lite-client/crypto/smartcont/stdlib.fc msg_hex_comment.fc token.fc &&
-
-~/liteclient-build/crypto/fift -I/Users/tolyayanot/lite-client/crypto/fift/lib -s test_dex.fif
--- transfer-dex.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-~/liteclient-build/crypto/fift -I/Users/tolyayanot/lite-client/crypto/fift/lib -s transfer-dex.fif dex.addr 0.3 0.2 0 0
